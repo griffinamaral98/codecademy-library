18adbe114d779a3610770ac9dbc5b184a51a516b
Griffin Amaral <griffin.amaral98@gmail.com>
2023-08-02 14:24:46 -0400

    Added Book subclass

--- index.js
+++ index.js
@@ -42,3 +42,20 @@ class Media {
     this._ratings.push(rating);
   }
 }
+
+// Book subclass that inherits props and functions from Media
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
